8ab446a66854e3c8227400aaea89bb202cdfcac1
nt_fox <nt_fox@foxmail.com>
2024-12-09 23:05:05 +0800

    body 绘制

--- Assets/script/Editor/BossEditor.cs
+++ Assets/script/Editor/BossEditor.cs
@@ -8,11 +8,29 @@ namespace GoFire
     [CustomEditor(typeof(Boss))]
     public class BossEditor : Editor
     {
+        private GUIStyle _labelStyle;
+        void OnEnable()
+        {
+
+        }
         void OnSceneGUI()
         {
-            //var boss = (Boss)target;
-            
+            var boss = (Boss)target;
 
+            if (_labelStyle == null)
+            {
+                _labelStyle = new GUIStyle(GUI.skin.label);
+                _labelStyle.fontSize = EditorConst.LabelFontSize;
+                _labelStyle.normal.textColor = Color.red;    
+            }
+            
+            Vector3[] lines = new Vector3[2];
+            var body = boss.GetComponent<IBody>();
+            //Handles.Slider(body.GameObject.transform.position + posOffset, -EditorConst.SceneUp, 1.0f, Handles.ArrowHandleCap, 0);
+            lines[0] = body.GameObject.transform.position + EditorConst.SceneTipLineHeightOffset;
+            lines[1] = new Vector3(lines[0].x, 0, lines[0].z);
+            Handles.Label(lines[0], body.GameObject.name, _labelStyle);
+            Handles.DrawLines(lines);
         }
     }
 
